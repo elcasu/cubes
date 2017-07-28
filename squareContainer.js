a529ca822443266a9bc113490e66784ca51232f2
Gabriel Casullo <info@casu-net.com.ar>
2017-07-28 05:03:31 -0300

    v5 - atractor with more strength

--- squareContainer.js
+++ squareContainer.js
@@ -12,17 +12,12 @@ const SquareContainer = function() {
   }
 }
 
+let s = 0.3
 SquareContainer.prototype.update = function() {
   const atractor = this.atractor
-  let s = 0.1
-  let dir = -1
   this.squares.forEach(function(sq) {
     const atractorForce = atractor.getForce(sq.cube.position, s)
     sq.applyForce(atractorForce)
     sq.update();
-    s += dir * 0.0001
-    if(s < 0 || s > 0.1) {
-      dir *= -1
-    }
   });
 }
